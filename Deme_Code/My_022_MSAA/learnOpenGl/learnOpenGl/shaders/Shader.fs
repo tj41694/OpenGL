36a48a534e4c59a170b793b44094991713f40769
tj41694 <tj41694@users.noreply.github.com>
2017-11-16 10:47:11 +0800

    2017-11-16

---
+++ Deme_Code/My_022_MSAA/learnOpenGl/learnOpenGl/shaders/Shader.fs
@@ -0,0 +1,14 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+in vec3 Normal;
+
+uniform sampler2D texture_diffuse1;
+
+void main()
+{    
+	FragColor = texture(texture_diffuse1, TexCoords);
+	
+	//FragColor = vec4(0.0f, 1.0f, 0.0f, 1.0f);  //纯色
+}
